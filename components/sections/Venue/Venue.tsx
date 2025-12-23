9c69289accce44b449a8f745d7c9530ce3b9d0ca
kiki <krishnakiran8684@gmail.com>
2025-12-23 10:11:12 +0530

    Removed border radius

--- components/sections/Venue/Venue.tsx
+++ components/sections/Venue/Venue.tsx
@@ -81,7 +81,7 @@ const Venue = () => {
                             src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3822.846566417743!2d81.83646351187428!3d16.767191283415983!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTbCsDQ2JzAxLjkiTiA4McKwNTAnMjAuNSJF!5e0!3m2!1sen!2sin!4v1703241234567!5m2!1sen!2sin"
                             width="100%"
                             height="450"
-                            style={{ border: 0, borderRadius: 'var(--radius-xl)' }}
+                            style={{ border: 0 }}
                             allowFullScreen
                             loading="lazy"
                             referrerPolicy="no-referrer-when-downgrade"
